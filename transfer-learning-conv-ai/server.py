cef24fd12652d0e2b76c207ccaf64caf613b65bd
Juan <sjdonado@uninorte.edu.co>
2020-08-26 17:36:00 -0500

    feat: flask server

---
+++ transfer-learning-conv-ai/server.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+from interact import get_answer
+
+app = Flask(__name__)
+
+history = []
+
+@app.route('/talk')
+def talk():
+    text = request.args.get('text')
+    get_answer(history, text)
